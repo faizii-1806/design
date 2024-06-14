d0a0259e55c865394ca43c1a3158dfb8f56690e4
faizii-1806 <159518287+faizii-1806@users.noreply.github.com>
2024-06-14 15:38:33 +0530

    Add files via upload

---
+++ design-1 - Copy/src/app/login/login.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AccessdeniedComponent } from './accessdenied/accessdenied.component';
+
+
+@NgModule({
+  declarations: [
+    LoginpageComponent,
+    AccessdeniedComponent
+  ],
+  imports: [
+    CommonModule,ReactiveFormsModule
+  ],
+  exports:[
+    LoginpageComponent,
+    AccessdeniedComponent
+  ]
+})
+export class LoginModule { }
